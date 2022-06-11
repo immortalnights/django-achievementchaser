c30cfcdc9a7e6c59bfb44c32f7e9f935011fafbc
Andrew <github@seventh.space>
2022-06-11 21:58:25 +0100

    feat: clean up admin pages to display model data (#2)

--- games/admin.py
+++ games/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Game, GameAchievement
 
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    """"""
+    list_display = ("name", "resynchronized", "resynchronization_required")
+
+
 # Register your models here.
-admin.site.register(Game)
 admin.site.register(GameAchievement)
