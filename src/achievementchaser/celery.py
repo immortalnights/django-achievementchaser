1ba0a693f1b484701e2380eab5060cfd329d2526
Andrew <github@seventh.space>
2023-08-06 14:54:04 +0100

    include celery beat

--- src/achievementchaser/celery.py
+++ src/achievementchaser/celery.py
@@ -1,5 +1,7 @@
 import os
+import logging
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "achievementchaser.settings")
@@ -14,8 +16,3 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f"Request: {self.request!r}")
