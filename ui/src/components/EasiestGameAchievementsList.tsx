c14fce856b3071b98fda79ebd9aba891caf243f3
Andrew <github@seventh.space>
2023-08-29 22:50:39 +0100

    tidy up next achievement display

--- ui/src/components/EasiestGameAchievementsList.tsx
+++ ui/src/components/EasiestGameAchievementsList.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import { Link } from "react-router-dom"
+import Grid from "@mui/material/Unstable_Grid2"
 import BorderedImage from "./BorderedImage"
 import FlexUnorderedList from "./FlexUnorderedList"
 import { Box } from "@mui/material"
@@ -43,47 +44,46 @@ const EasiestGameAchievementsList = ({
     }, [achievements])
 
     return (
-        <Box
-            sx={{
-                display: "flex",
-                flexWrap: "wrap",
-                gap: 2,
-                justifyContent: "space-between",
-            }}
-        >
+        <Grid container>
             {Object.entries(groupedAchievements).map(([id, item]) => (
-                <div style={{ maxWidth: 220, textAlign: "center" }} key={id}>
-                    <Link
-                        to={`/player/${player}/game/${item.id}`}
-                        title={item.name}
-                    >
-                        <BorderedImage
-                            src={`https://media.steampowered.com/steam/apps/${item.id}/capsule_184x69.jpg`}
-                        />
-                    </Link>
-                    <FlexUnorderedList justifyContent="center" wrap>
-                        {item.achievements.map((achievement) => (
-                            <li
-                                key={`${achievement.game?.name}-${achievement.name}`}
-                                title={`${
-                                    achievement.displayName
-                                } - ${achievement.globalPercentage?.toFixed(
-                                    2
-                                )}%`}
-                            >
-                                <BorderedImage
-                                    src={achievement.iconUrl}
-                                    style={{
-                                        width: 64,
-                                        height: 64,
-                                    }}
-                                />
-                            </li>
-                        ))}
-                    </FlexUnorderedList>
-                </div>
+                <Grid
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    lg={4}
+                    display="flex"
+                    gap={1}
+                    wrap="wrap"
+                >
+                    <Box>
+                        <Link
+                            to={`/player/${player}/game/${item.id}`}
+                            title={item.name}
+                        >
+                            <BorderedImage
+                                src={`https://media.steampowered.com/steam/apps/${item.id}/capsule_184x69.jpg`}
+                            />
+                        </Link>
+                    </Box>
+                    {item.achievements.map((achievement) => (
+                        <div
+                            key={`${achievement.game?.name}-${achievement.name}`}
+                            title={`${
+                                achievement.displayName
+                            } - ${achievement.globalPercentage?.toFixed(2)}%`}
+                        >
+                            <BorderedImage
+                                src={achievement.iconUrl}
+                                style={{
+                                    width: 64,
+                                    height: 64,
+                                }}
+                            />
+                        </div>
+                    ))}
+                </Grid>
             ))}
-        </Box>
+        </Grid>
     )
 }
 
