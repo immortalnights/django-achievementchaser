f430ccb3b91c8294d8afa2af137d4391d91bacca
Andrew <github@seventh.space>
2023-09-01 01:43:18 +0100

    remove custom player related queries

--- src/players/schema/__init__.py
+++ src/players/schema/__init__.py
@@ -1,16 +1,10 @@
 from graphene import ObjectType
 from .players import Query as PlayerQuery
-from .profiles import Query as ProfileQuery
-from .ownedgames import Query as OwnedGameQuery
-from .achievements import Query as AchievementQuery
 from .mutations import ResynchronizePlayer, ResynchronizePlayerGame
 
 
 class Queries(
     PlayerQuery,
-    ProfileQuery,
-    OwnedGameQuery,
-    AchievementQuery,
     ObjectType,
 ):
     pass
