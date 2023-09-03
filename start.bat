9a39fefbb1187e5d557700ec26f5a8c5b8b4bbb7
Andrew <github@seventh.space>
2023-09-03 21:22:01 +0100

    do not start celery

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 :: .\.venv\Scripts\activate.bat
 cd src\
 start python .\manage.py runserver
-start celery --app achievementchaser worker --loglevel INFO -P solo
-start celery --app achievementchaser beat
+@REM start celery --app achievementchaser worker --loglevel INFO -P solo
+@REM start celery --app achievementchaser beat
 cd ..\ui\
 start npm run dev
