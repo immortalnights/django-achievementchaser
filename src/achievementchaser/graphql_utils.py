ae1f5b522d71bd52d1903030640a966c2c764f42
Andrew <github@seventh.space>
2023-09-03 01:28:49 +0100

    update graphql utils

--- src/achievementchaser/graphql_utils.py
+++ src/achievementchaser/graphql_utils.py
@@ -2,6 +2,18 @@ from typing import List, Dict
 from graphql import FieldNode
 
 
+def parse_order_by(value: str):
+    values = value.split(" ")[:2]
+    if len(values) < 2:
+        values.append("ASC")
+    elif values[1] not in ("ASC", "DESC"):
+        values[1] = "ASC"
+
+    order_modifier = "" if values[1] == "ASC" else "-"
+
+    return (values[0], order_modifier)
+
+
 def get_field_hierarchy(fields: List[FieldNode]) -> Dict:
     """Iterate the fields building an object of field name to subfields."""
     obj = {}
@@ -19,11 +31,6 @@ def get_field_selection_hierarchy(fields) -> Dict:
     return get_field_hierarchy(fields[0].selection_set.selections)
 
 
-def get_edge_fields(fields: Dict) -> List:
+def get_edge_node_fields(fields: Dict) -> Dict:
     """If the field hierarchy contains an edge and node field, return the selected edge node fields."""
-    res = None
-    if fields and "edges" in fields:
-        if "node" in fields["edges"]:
-            res = list(fields["edges"]["node"].keys())
-
-    return res
+    return fields["edges"]["node"] if fields and "edges" in fields and "node" in fields["edges"] else {}
