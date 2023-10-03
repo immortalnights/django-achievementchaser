37bf7f5f9ca765bc1df45957b9fe8e7050b6ed4e
Andrew <github@seventh.space>
2023-10-04 00:07:43 +0100

    feature: add format date utility function

--- ui/src/utilities.ts
+++ ui/src/utilities.ts
@@ -4,6 +4,8 @@ import relativeTime from "dayjs/plugin/relativeTime"
 export const throwExpression = (errorMessage: string): never => {
     throw new Error(errorMessage)
 }
+export const formatDate = (date: string | dayjs.Dayjs) =>
+    dayjs(date).format("MMM D, YYYY")
 
 export const getRelativeTime = (date: string | dayjs.Dayjs) => {
     dayjs.extend(relativeTime)
