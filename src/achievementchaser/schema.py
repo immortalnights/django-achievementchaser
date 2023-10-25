d1856c638aab94be141918433c618d6215e03578
Andrew <github@seventh.space>
2023-10-25 22:34:06 +0100

    feature: add Search query endpoint

--- src/achievementchaser/schema.py
+++ src/achievementchaser/schema.py
@@ -2,11 +2,29 @@ import graphene
 from graphene_django import DjangoObjectType  # noqa F401
 from players.schema import Queries as PlayerQueries, ResynchronizePlayer, ResynchronizePlayerGame
 from games.schema import Queries as GameQueries, ResynchronizeGame
+from players.schema.players import Player, PlayerType
+from games.schema.games import Game, GameType
+
+
+class ResultType(graphene.Union):
+    class Meta:
+        types = (PlayerType, GameType)
+
+
+class Search(graphene.ObjectType):
+    search_players_and_games = graphene.List(ResultType, name=graphene.String())
+
+    def resolve_search_players_and_games(self, info, name):
+        players = Player.objects.filter(name__icontains=name)
+        games = Game.objects.filter(name__icontains=name)
+
+        return list(players) + list(games)
 
 
 class Queries(
     PlayerQueries,
     GameQueries,
+    Search,
     graphene.ObjectType,
 ):
     pass
