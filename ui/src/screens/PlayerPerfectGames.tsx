a4d46c8289deabfffd7f4ae0de4f85412c4e831a
Andrew <github@seventh.space>
2023-11-08 00:28:53 +0000

    fix: optimize owned game queries and components

--- ui/src/screens/PlayerPerfectGames.tsx
+++ ui/src/screens/PlayerPerfectGames.tsx
@@ -1,35 +1,19 @@
-import { useParams } from "react-router-dom"
-import { useQueryPlayerGames } from "../api/queries"
-import Loader from "../components/Loader"
-import { throwExpression } from "../utilities"
+import { useRouteLoaderData } from "react-router-dom"
 import { Typography } from "@mui/material"
-import PlayerOwnedGames from "../components/PlayerOwnedGames"
+import LoadPlayerOwnedGames from "../components/LoadPlayerOwnedGames"
 
 const PlayerPerfectGames = () => {
-    const { id: player = throwExpression("missing param") } = useParams()
-    const { loading, error, data } = useQueryPlayerGames({
-        player,
-        completed: true,
-        orderBy: "-completed",
-    })
+    const player = useRouteLoaderData("player") as Player
 
     return (
-        <Loader
-            loading={loading}
-            error={error}
-            data={data}
-            renderer={(ownedGames) => {
-                return (
-                    <>
-                        <Typography variant="h5">Perfect Games</Typography>
-                        <PlayerOwnedGames
-                            player={player}
-                            ownedGames={ownedGames}
-                        />
-                    </>
-                )
-            }}
-        />
+        <>
+            <Typography variant="h5">Perfect Games</Typography>
+            <LoadPlayerOwnedGames
+                player={player.id}
+                completed={true}
+                order="-completed"
+            />
+        </>
     )
 }
 
--- ui/src/screens/PlayerProfile.tsx
+++ ui/src/screens/PlayerProfile.tsx
@@ -1,47 +1,9 @@
-import { useLoaderData, useParams, useRouteLoaderData } from "react-router-dom"
+import { useRouteLoaderData } from "react-router-dom"
 import { Typography } from "@mui/material"
-import {
-    useQueryPlayer,
-    useQueryPlayerGames,
-    useQueryPlayerAchievements,
-    useQueryPlayerOwnedGames,
-} from "../api/queries"
+import { useQueryPlayerAchievements } from "../api/queries"
 import Loader from "../components/Loader"
-import { throwExpression } from "../utilities"
-import OwnedGameList from "../components/OwnedGameList"
 import GameAchievementsList from "../components/GameAchievementsList"
-import PlayerOwnedGames from "../components/PlayerOwnedGames"
-
-const PlayerOwnedGamesList = ({
-    player,
-    started,
-    completed,
-    order,
-}: {
-    player: string
-    started?: boolean
-    completed?: boolean
-    order?: string
-}) => {
-    const { loading, error, data } = useQueryPlayerGames({
-        player,
-        started,
-        completed,
-        orderBy: order,
-        limit: 12,
-    })
-
-    return (
-        <Loader
-            loading={loading}
-            error={error}
-            data={data}
-            renderer={(games) => {
-                return <PlayerOwnedGames player={player} ownedGames={games} />
-            }}
-        />
-    )
-}
+import LoadPlayerOwnedGames from "../components/LoadPlayerOwnedGames"
 
 const PlayerGameAchievementList = ({ player }: { player: string }) => {
     const { loading, error, data } = useQueryPlayerAchievements({
@@ -69,24 +31,25 @@ const PlayerGameAchievementList = ({ player }: { player: string }) => {
 
 const PlayerProfileScreen = () => {
     const player = useRouteLoaderData("player") as Player
+
     return (
         <>
             <Typography variant="h5">Almost There</Typography>
-            <PlayerOwnedGamesList
+            <LoadPlayerOwnedGames
                 player={player.id}
                 completed={false}
                 order="-completionPercentage"
             />
 
             <Typography variant="h5">Just Started</Typography>
-            <PlayerOwnedGamesList
+            <LoadPlayerOwnedGames
                 player={player.id}
                 started={true}
                 order="completionPercentage"
             />
 
             <Typography variant="h5">Next Game</Typography>
-            <PlayerOwnedGamesList
+            <LoadPlayerOwnedGames
                 player={player.id}
                 completed={false}
                 order="-game__difficulty_percentage"
