3f230c4396f0e7464f3bf457055f66528b490be3
Andrew <github@seventh.space>
2023-11-11 23:00:01 +0000

    fix: played owned game achievement query

--- src/players/schema/types.py
+++ src/players/schema/types.py
@@ -39,6 +39,9 @@ class PlayerOwnedGameNode(DjangoObjectType):
     unlocked_achievements = graphene.List(PlayerUnlockedAchievementNode)
     unlocked_achievement_count = graphene.Int()
 
+    def resolve_unlocked_achievements(root, info):
+        return PlayerUnlockedAchievement.objects.filter(player=root.player_id, game=root.game_id).order_by("-datetime")
+
     def resolve_unlocked_achievement_count(root, info):
         return PlayerUnlockedAchievement.objects.filter(player=root.player_id, game=root.game_id).count()
 
