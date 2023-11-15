f31ee7f74b365dfd4d67693b75670330b5d9e962
Andrew <github@seventh.space>
2023-11-15 00:13:55 +0000

    chore: add query documents and basic tests

---
+++ ui/src/api/documents.test.ts
@@ -0,0 +1,152 @@
+import { LocalGraphQLClient } from "graphql-hooks"
+import { test, expect } from "vitest"
+import { player, playerGames, players } from "./documents"
+
+test("players query", async () => {
+    const playersResponseData = {
+        data: {
+            players: [
+                {
+                    id: "00000000000000001",
+                    name: "Player1",
+                },
+                {
+                    id: "00000000000000002",
+                    name: "Player2",
+                },
+            ],
+        },
+    }
+
+    const client = new LocalGraphQLClient({
+        localQueries: {
+            [players]: () => playersResponseData,
+        },
+    })
+
+    const { data, error } = await client.request({ query: players })
+    expect(data).toEqual(playersResponseData)
+    expect(error).toBeUndefined()
+})
+
+test("player query", async () => {
+    const client = new LocalGraphQLClient({
+        localQueries: {
+            [player]: ({ player }) => ({
+                data: {
+                    player: {
+                        id: String(player),
+                        name: "Player1",
+                    },
+                },
+            }),
+        },
+    })
+
+    const { data, error } = await client.request({
+        query: player,
+        variables: { player: 1 },
+    })
+    expect(data).toEqual({
+        data: {
+            player: {
+                id: "1",
+                name: "Player1",
+            },
+        },
+    })
+    expect(error).toBeUndefined()
+})
+
+test("player query error", async () => {
+    const client = new LocalGraphQLClient({
+        localQueries: {
+            [player]: ({ player }) => ({
+                errors: [
+                    {
+                        message: `Could not find Player with id '${player}' or name 'None'`,
+                        locations: [
+                            {
+                                line: 2,
+                                column: 3,
+                            },
+                        ],
+                        path: ["player"],
+                    },
+                ],
+                data: {
+                    player: null,
+                },
+            }),
+        },
+    })
+
+    const { data, error } = await client.request({
+        query: player,
+        variables: { player: 1 },
+    })
+    console.log(data, error)
+    expect(data.data.player).toBeNull()
+    expect(error).toBeUndefined()
+})
+
+test("player games query", async () => {
+    const client = new LocalGraphQLClient({
+        localQueries: {
+            [playerGames]: ({
+                player,
+                started,
+                completed,
+                year,
+                orderBy,
+                limit,
+            }) => ({
+                data: {
+                    player: {
+                        id: String(player),
+                        name: "Player1",
+                        games: {
+                            edges: [
+                                {
+                                    node: {
+                                        id: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                },
+            }),
+        },
+    })
+
+    const { data, error } = await client.request({
+        query: playerGames,
+        variables: {
+            player: 1,
+            started: true,
+            completed: true,
+            year: 2010,
+            orderBy: "-completed",
+            limit: 10,
+        },
+    })
+    expect(data).toEqual({
+        data: {
+            player: {
+                id: "1",
+                name: "Player1",
+                games: {
+                    edges: [
+                        {
+                            node: {
+                                id: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+                            },
+                        },
+                    ],
+                },
+            },
+        },
+    })
+    expect(error).toBeUndefined()
+})
