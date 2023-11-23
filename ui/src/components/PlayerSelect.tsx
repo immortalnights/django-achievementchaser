df76b0572eb82531d33037a20c858d521c1ae2d5
Andrew <github@seventh.space>
2023-11-23 00:48:20 +0000

    chore: split PlayerSelect to own component

---
+++ ui/src/components/PlayerSelect.tsx
@@ -0,0 +1,60 @@
+import {
+    SelectChangeEvent,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+} from "@mui/material"
+import { useQuery } from "graphql-hooks"
+import { useContext } from "react"
+import { players as playersDocument } from "../api/documents"
+import PlayerCompareContext from "../context/PlayerCompareContext"
+
+const PlayerSelect = ({
+    filter,
+    value,
+}: {
+    filter: string[]
+    value?: string
+}) => {
+    // TODO add game filter
+    const { data, loading, error } =
+        useQuery<PlayersQueryResponse>(playersDocument)
+    const { setOtherPlayer } = useContext(PlayerCompareContext)
+
+    const handleChange = (event: SelectChangeEvent<string>) => {
+        setOtherPlayer(event.target.value)
+    }
+
+    let players: Player[] = []
+    if (data) {
+        players = data.players.filter((player) => !filter.includes(player.id))
+    }
+
+    return (
+        <FormControl sx={{ minWidth: 200 }} size="small">
+            <InputLabel id="select-player-compare-label">Compare</InputLabel>
+            <Select
+                labelId="select-player-compare-label"
+                fullWidth
+                value={(data && value) ?? ""}
+                label="Compare"
+                size="small"
+                onChange={handleChange}
+                disabled={loading}
+            >
+                {value && (
+                    <MenuItem key="noone" value="">
+                        No one
+                    </MenuItem>
+                )}
+                {players.map((player) => (
+                    <MenuItem key={player.id} value={player.id}>
+                        {player.name}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    )
+}
+export default PlayerSelect
