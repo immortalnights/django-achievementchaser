a7e93cd5b451be7c173a8181e7a513c443c7da06
Andrew <github@seventh.space>
2024-02-23 02:21:51 +0000

    chore: add snapshot tests for CircularProgress

---
+++ ui/src/components/CircularProgressWithLabel/CircularProgressWithLabel.test.tsx
@@ -0,0 +1,10 @@
+import renderer from "react-test-renderer"
+import { expect, test } from "vitest"
+import CircularProgressWithLabel from "."
+
+test("snapshot", () => {
+    const component = renderer.create(
+        <CircularProgressWithLabel value={50} color="primary" />
+    )
+    expect(component.toJSON()).toMatchSnapshot()
+})
