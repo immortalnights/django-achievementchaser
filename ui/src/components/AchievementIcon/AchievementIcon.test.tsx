07bb874122aa25ee3adc9788941b217fff76f869
Andrew <github@seventh.space>
2024-02-23 02:21:28 +0000

    chore: add snapshot test for AchievementIcon

---
+++ ui/src/components/AchievementIcon/AchievementIcon.test.tsx
@@ -0,0 +1,20 @@
+import renderer from "react-test-renderer"
+import { expect, test } from "vitest"
+import AchievementIcon from "."
+import { achievement } from "@test/data"
+
+test("snapshot", () => {
+    const achievementIcon = renderer.create(
+        <AchievementIcon game="Test Game" achievement={achievement} />
+    )
+    expect(achievementIcon.toJSON()).toMatchSnapshot()
+
+    const unlockedAchievementIcon = renderer.create(
+        <AchievementIcon
+            game="Test Game"
+            achievement={achievement}
+            unlockedDatetime="2015-03-09T01:17:41+00:00"
+        />
+    )
+    expect(unlockedAchievementIcon.toJSON()).toMatchSnapshot()
+})
