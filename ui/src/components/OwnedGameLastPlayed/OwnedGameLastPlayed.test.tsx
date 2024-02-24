1038f1855a3aa6ec22ad82d13fa6ba2fc547a3ba
Andrew <github@seventh.space>
2024-02-24 21:34:29 +0000

    fix: tests

--- ui/src/components/OwnedGameLastPlayed/OwnedGameLastPlayed.test.tsx
+++ ui/src/components/OwnedGameLastPlayed/OwnedGameLastPlayed.test.tsx
@@ -2,11 +2,8 @@ import renderer from "react-test-renderer"
 import { expect, test, vi } from "vitest"
 import OwnedGameLastPlayed from "."
 import { playedPlayerOwnedGame } from "@test/data"
-import { setup } from "@/dayjsUtilities"
 
 test("snapshot", () => {
-    setup()
-
     vi.setSystemTime("2024-02-23T12:00:00Z")
 
     const component = renderer.create(
--- ui/src/components/OwnedGamePlaytime/OwnedGamePlaytime.test.tsx
+++ ui/src/components/OwnedGamePlaytime/OwnedGamePlaytime.test.tsx
@@ -2,11 +2,8 @@ import renderer from "react-test-renderer"
 import { expect, test } from "vitest"
 import OwnedGamePlaytime from "."
 import { playedPlayerOwnedGame } from "@test/data"
-import { setup } from "@/dayjsUtilities"
 
 test("snapshot", () => {
-    setup()
-
     const component = renderer.create(
         <OwnedGamePlaytime {...playedPlayerOwnedGame} />
     )
