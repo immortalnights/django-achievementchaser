bd3ac71b2c2a9dcc5725fe1b227fde02cfd4ade6
Andrew <github@seventh.space>
2024-02-24 13:58:54 +0000

    fix: use dayjs instead of custom duration

--- ui/src/utilities.ts
+++ ui/src/utilities.ts
@@ -5,21 +5,3 @@ export const throwExpression = (errorMessage: string): never => {
 //! return a percentage string for two numbers
 export const percentage = (a: number, b: number) =>
     `${((a / b) * 100).toFixed(2)}%`
-
-//! duration
-export const playtime = (playtime: number) => {
-    const units = { minutes: 1, hrs: 60, days: 24, years: 365 } as const
-    const keys = Object.keys(units)
-    let value = playtime
-    let index = 0
-    for (; index < keys.length; index++) {
-        const divider = units[keys[index] as keyof typeof units]
-        if (value > divider) {
-            value = value / divider
-        } else {
-            break
-        }
-    }
-
-    return `${index > 1 ? value.toFixed(2) : value} ${keys[index - 1] ?? ""}`
-}
