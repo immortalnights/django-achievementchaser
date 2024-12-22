0f4313f9d481e037ee408259abc80c8c55b1e986
Andrew <github@seventh.space>
2024-12-22 12:34:20 +0000

    chore: functionize patch of steam request function

--- src/achievementchaser/test_utilities.py
+++ src/achievementchaser/test_utilities.py
@@ -2,14 +2,18 @@ from unittest.mock import patch
 from typing import Any
 
 
+def patch_request():
+    return patch("achievementchaser.steam._request")
+
+
 def mock_request(*, ok=True, data: Any = None):
     def inner(fn):
         def test_internal(self):
-            mock = patch("achievementchaser.steam._request")
-            mock_request = mock.start()
+            _patch = patch_request()
+            mock_request = _patch.start()
             mock_request.return_value = (ok, data)
             res = fn(self, mock_request=mock_request)
-            mock.stop()
+            _patch.stop()
             return res
 
         return test_internal
