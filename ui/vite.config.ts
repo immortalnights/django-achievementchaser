3bfb335db6eb2a58249648540fc9e9c107ed3d31
Andrew <github@seventh.space>
2025-03-07 11:21:29 +0000

    chore: add caddyfile chore: add caddy script to package

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -18,7 +18,6 @@ export default defineConfig({
         proxy: {
             "/graphql": {
                 target: "http://127.0.0.1:8000",
-                // changeOrigin: true,
             },
         },
     },
